3535dc2f608282b02fdbd7a10f019edfacea068d
Kalpesh Dhoke <kalpeshdhoke7@gmail.com>
2025-02-05 17:30:48 +0530

    Add React Spring trail animation for component rendering
    - Implement animated grid items using @react-spring/web
    - Create trail animation for App component's grid items
    - Enhance component rendering with smooth entrance animation
    - Add configuration for animation properties and scaling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Container, Grid, GridItem } from "@chakra-ui/react";
 import { useEffect } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
+import { useTrail, animated } from "@react-spring/web";
 import Counter from "./components/Counter";
 import RichTextEditor from "./components/RichTextEditor";
 import { RootState } from "./store/store";
 import UserData from "./components/UserData";
 import ContactDetails from "./components/ContactDetails";
+
+// Animated version of GridItem
+const AnimatedGridItem = animated(GridItem);
 
 function App() {
   const editorHasUnsavedChanges = useSelector(
@@ -14,6 +19,32 @@ function App() {
   const userHasUnsavedChanges = useSelector(
     (state: RootState) => state.user.hasUnsavedChanges
   );
+
+  // Create a trail animation for multiple items
+  const items = [
+    { component: Counter },
+    { component: RichTextEditor },
+    { component: UserData },
+    { component: ContactDetails },
+  ];
+
+  const trail = useTrail(items.length, {
+    from: {
+      opacity: 0,
+      transform: "scale(0.95) translateY(10px)",
+    },
+    to: {
+      opacity: 1,
+      transform: "scale(1) translateY(0px)",
+    },
+    config: {
+      mass: 1,
+      tension: 170,
+      friction: 26,
+      clamp: false,
+      velocity: 0,
+    },
+  });
 
   useEffect(() => {
     const handleBeforeUnload = (e: BeforeUnloadEvent) => {
@@ -30,18 +61,11 @@ function App() {
   return (
     <Container maxW="container.xl" py={6}>
       <Grid templateColumns="repeat(2, 1fr)" gap={6}>
-        <GridItem>
-          <Counter />
-        </GridItem>
-        <GridItem>
-          <RichTextEditor />
-        </GridItem>
-        <GridItem>
-          <UserData />
-        </GridItem>
-        <GridItem>
-          <ContactDetails />
-        </GridItem>
+        {trail.map((style, index) => (
+          <AnimatedGridItem key={index} style={style}>
+            {React.createElement(items[index].component)}
+          </AnimatedGridItem>
+        ))}
       </Grid>
     </Container>
   );
